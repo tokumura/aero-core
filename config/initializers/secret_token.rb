6ebb5ce4d53553faddfa5a7abc7ae8c1f6deba8b
Tsuyoshi Okumura <qkrhn081@gmail.com>
2012-06-01 12:21:05 +0900

    rails new

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AeroCore::Application.config.secret_token = '07319ff5e9af6534360999cb8afaca95eadaf25230d3d242a8078fac6c63f5f5b781e85f653210ee041243e0fb4b8585cf9facbd44d5608f87849dcc159a8890'
