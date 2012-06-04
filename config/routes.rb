31c61f09e9f0d2648a488de7c8a3aeeee9dc48e5
Tsuyoshi Okumura <qkrhn081@gmail.com>
2012-06-04 18:03:08 +0900

    [#30462119] Deviseを利用した認証機能を追加. ユーザー名での認証にカスタマイズ.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 AeroCore::Application.routes.draw do
   resources :products
+  resources :welcome, :only => ['index']
 
   devise_for :users
 
@@ -52,7 +53,9 @@ AeroCore::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => "products#index"
+  root :to => "welcome#index"
+  get 'products', :to => 'products#index', :as => :user_root
+  
 
   # See how all your routes lay out with "rake routes"
 
