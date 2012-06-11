6a5ccc3e40864fe9be02a23dee5833a30dcda6e8
Tsuyoshi Okumura <qkrhn081@gmail.com>
2012-06-11 10:46:31 +0900

    テストコード修正

--- spec/views/products/show.html.erb_spec.rb
+++ spec/views/products/show.html.erb_spec.rb
@@ -2,14 +2,28 @@
 require 'spec_helper'
 
 describe "products/show.html.erb" do
-  fixtures :departments, :products
+  fixtures :categories, :departments, :products
   before do
     @product = Product.find(1)
+    @department_name = Array.new(@product.departments.size)
+    @product.departments.each do |d|
+      department = Department.find(d.id)
+      @department_name << department.name
+    end
+
+    @category_name = Array.new(@product.categories.size)
+    @product.categories.each do |c|
+      category = Category.find(c.id)
+      puts category.name
+      @category_name << category.name
+    end
   end
+
   it "商品名'LBP9100'が表示される。" do
     render
     rendered.should have_content("LBP9100")
   end
+
   it "価格'50000'が表示される。" do
     render
     rendered.should have_content("50000")
